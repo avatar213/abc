bb0246b81517f942eed902d43722b7e83ea7263e
avatar213 <134165331+avatar213@users.noreply.github.com>
2025-09-21 10:00:31 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let name;
-
-document.getElementById("myButton").onclick = function(){
-	name = document.getElementById("myText").value;
-	console.log(name);
-	document.getElementById("myLabel").innerHTML = name + " молодец, а Елнара не знает немецкий в 2023 году";
-}
